dea89e747023d232f23fda46e252d2258411d1ab
Adnan Khan <adnanonline109@gmail.com>
2024-11-21 19:38:04 +0500

    Updated [specific feature/fix]

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,5 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from "react";
-import PropTypes from "prop-types";
+
 
 const TodoList = ({todos, onDelete, onEdit }) => {
   return (
